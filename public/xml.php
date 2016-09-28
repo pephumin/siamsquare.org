3b8e853fa41564bd3eeb6e92d150abf2bc5a1d05
Phumin Chesdmethee <phumin@sawasdee.org>
2016-09-28 10:33:28 +0700

    Testing xml schema
    Testing xml schema

---
+++ public/xml.php
@@ -0,0 +1,29 @@
+<?php
+
+$_SERVER['BASE_PAGE'] = 'xml.php';
+$title = "XML database pulling";
+require_once $_SERVER['DOCUMENT_ROOT'].'/admin/assets/include/config.php';
+require_once DOCROOT.'/public/assets/include/template.php';
+require_once DOCROOT.'/public/assets/include/first.php';
+
+pageHeader($title);
+
+// --------------------------------------------------------------------------------
+
+$p = new SimpleXMLElement(file_get_contents("assets/xml/provinces_eng.xml"));
+
+foreach ($p->children() as $ps) {
+  echo $ps->title;
+  echo "<br>\n";
+  foreach ($ps->title->provinces as $ss) {
+    echo $ss->province;
+    echo "<br>\n";
+  }
+}
+
+// --------------------------------------------------------------------------------
+
+if ($notes) { pageFooter($notes); } else { pageFooter(); }
+
+
+?>
