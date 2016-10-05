c38574006dfd208a70c38ab33f649bbc5f629222
Phumin Chesdmethee <phumin@sawasdee.org>
2016-10-05 22:28:03 +0700

    Minor clean up
    Minor clean up

--- public/contact.php
+++ public/contact.php
@@ -35,68 +35,66 @@ echo "<br>\n";
 
 ?>
 
-<form class="form-horizontal" role="form" method="post" action="<?php echo htmlspecialchars(ME); ?>">
-  <div id="contactus">
-    <div class="form-group">
-      <label for="name" class="col-xs-12 col-sm-3 col-md-3 col-lg-3 control-label">ชื่อของคุณ</label>
-      <div class="col-xs-12 col-sm-7 col-md-7 col-lg-7">
-        <div class="input-group">
-          <span class="input-group-addon"><i class="pe-user pe-fw"></i></span>
-          <?php if ($sent) { ?>
-          <input type="text" class="form-control" name="name" placeholder="ชื่อและนามสกุลของคุณ" value="<?php echo htmlspecialchars($_REQUEST['name']); ?>" disabled>
-          <?php } else { ?>
-          <input type="text" class="form-control" name="name" placeholder="ชื่อและนามสกุลของคุณ" value="<?php echo htmlspecialchars($_REQUEST['name']); ?>">
-          <?php } ?>
-        </div>
-      </div>
-    </div>
-    <div class="form-group">
-      <label for="name" class="col-xs-12 col-sm-3 col-md-3 col-lg-3 control-label">อีเมล์ของคุณ</label>
-      <div class="col-xs-12 col-sm-7 col-md-7 col-lg-7">
-        <div class="input-group">
-          <span class="input-group-addon"><i class="pe-envelope pe-fw"></i></span>
-          <?php if ($sent) { ?>
-          <input type="email" class="form-control" name="email" placeholder="email@company.com" value="<?php echo htmlspecialchars($_REQUEST['email']); ?>" disabled>
-          <?php } else { ?>
-          <input type="email" class="form-control" name="email" placeholder="email@company.com" value="<?php echo htmlspecialchars($_REQUEST['email']); ?>">
-          <?php } ?>
-        </div>
-      </div>
-    </div>
-    <div class="form-group">
-      <label for="name" class="col-xs-12 col-sm-3 col-md-3 col-lg-3 control-label">ข้อความ</label>
-      <div class="col-xs-12 col-sm-7 col-md-7 col-lg-7">
-        <div class="input-group">
-          <span class="input-group-addon input-group-addon-top"><i class="pe-commenting pe-fw"></i></span>
-          <?php if ($sent) { ?>
-          <textarea class="form-control" rows="7" name="message" placeholder="คุณสามารถพิมพ์ข้อความได้ยาวเท่าที่ต้องการในช่องนี้" disabled><?php echo htmlspecialchars($_REQUEST['message']);?></textarea>
-          <?php } else { ?>
-          <textarea class="form-control" rows="7" name="message" placeholder="คุณสามารถพิมพ์ข้อความได้ยาวเท่าที่ต้องการในช่องนี้"><?php echo htmlspecialchars($_REQUEST['message']);?></textarea>
-          <?php } ?>
-        </div>
-      </div>
-    </div>
-    <div class="form-group">
-      <label for="name" class="col-xs-12 col-sm-3 col-md-3 col-lg-3 control-label">2 + 3 เท่ากับ?</label>
-      <div class="col-xs-12 col-sm-7 col-md-7 col-lg-7">
-        <div class="input-group">
-          <span class="input-group-addon"><i class="pe-question pe-fw"></i></span>
-          <?php if ($sent) { ?>
-          <input type="text" class="form-control" name="human" placeholder="คำตอบของ 2 + 3 คือ..." value="<?php echo htmlspecialchars($_REQUEST['human']); ?>" disabled>
-          <?php } else { ?>
-          <input type="text" class="form-control" name="human" placeholder="คำตอบของ 2 + 3 คือ..." value="<?php echo htmlspecialchars($_REQUEST['human']); ?>">
-          <?php } ?>
-        </div>
-      </div>
-    </div>
-    <div class="form-group">
-      <div class="col-xs-offset-0 col-sm-offset-3 col-md-offset-3 col-lg-offset-3 col-xs-12 col-sm-7 col-md-7 col-lg-7">
+<form id="contactus" class="form-horizontal" role="form" method="post" action="<?php echo htmlspecialchars(MYPUBLIC); ?>">
+  <div class="form-group">
+    <label for="name" class="col-xs-12 col-sm-3 col-md-3 col-lg-3 control-label">ชื่อของคุณ</label>
+    <div class="col-xs-12 col-sm-7 col-md-7 col-lg-7">
+      <div class="input-group">
+        <span class="input-group-addon"><i class="pe-user pe-fw"></i></span>
         <?php if ($sent) { ?>
-        <button type="submit" class="btn btn-info" disabled>ส่งข้อความ <i class="pe-paper-plane"></i></button>
+        <input type="text" class="form-control" name="name" placeholder="ชื่อและนามสกุลของคุณ" value="<?php echo htmlspecialchars($_REQUEST['name']); ?>" disabled>
         <?php } else { ?>
-        <button type="submit" class="btn btn-info">ส่งข้อความ <i class="pe-paper-plane"></i></button>
+        <input type="text" class="form-control" name="name" placeholder="ชื่อและนามสกุลของคุณ" value="<?php echo htmlspecialchars($_REQUEST['name']); ?>">
         <?php } ?>
       </div>
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="name" class="col-xs-12 col-sm-3 col-md-3 col-lg-3 control-label">อีเมล์ของคุณ</label>
+    <div class="col-xs-12 col-sm-7 col-md-7 col-lg-7">
+      <div class="input-group">
+        <span class="input-group-addon"><i class="pe-envelope pe-fw"></i></span>
+        <?php if ($sent) { ?>
+        <input type="email" class="form-control" name="email" placeholder="email@company.com" value="<?php echo htmlspecialchars($_REQUEST['email']); ?>" disabled>
+        <?php } else { ?>
+        <input type="email" class="form-control" name="email" placeholder="email@company.com" value="<?php echo htmlspecialchars($_REQUEST['email']); ?>">
+        <?php } ?>
+      </div>
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="name" class="col-xs-12 col-sm-3 col-md-3 col-lg-3 control-label">ข้อความ</label>
+    <div class="col-xs-12 col-sm-7 col-md-7 col-lg-7">
+      <div class="input-group">
+        <span class="input-group-addon input-group-addon-top"><i class="pe-commenting pe-fw"></i></span>
+        <?php if ($sent) { ?>
+        <textarea class="form-control" rows="7" name="message" placeholder="คุณสามารถพิมพ์ข้อความได้ยาวเท่าที่ต้องการในช่องนี้" disabled><?php echo htmlspecialchars($_REQUEST['message']);?></textarea>
+        <?php } else { ?>
+        <textarea class="form-control" rows="7" name="message" placeholder="คุณสามารถพิมพ์ข้อความได้ยาวเท่าที่ต้องการในช่องนี้"><?php echo htmlspecialchars($_REQUEST['message']);?></textarea>
+        <?php } ?>
+      </div>
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="name" class="col-xs-12 col-sm-3 col-md-3 col-lg-3 control-label">2 + 3 เท่ากับ?</label>
+    <div class="col-xs-12 col-sm-7 col-md-7 col-lg-7">
+      <div class="input-group">
+        <span class="input-group-addon"><i class="pe-question pe-fw"></i></span>
+        <?php if ($sent) { ?>
+        <input type="text" class="form-control" name="human" placeholder="คำตอบของ 2 + 3 คือ..." value="<?php echo htmlspecialchars($_REQUEST['human']); ?>" disabled>
+        <?php } else { ?>
+        <input type="text" class="form-control" name="human" placeholder="คำตอบของ 2 + 3 คือ..." value="<?php echo htmlspecialchars($_REQUEST['human']); ?>">
+        <?php } ?>
+      </div>
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-xs-offset-0 col-sm-offset-3 col-md-offset-3 col-lg-offset-3 col-xs-12 col-sm-7 col-md-7 col-lg-7">
+      <?php if ($sent) { ?>
+      <button type="submit" class="btn btn-info" disabled>ส่งข้อความ <i class="pe-paper-plane"></i></button>
+      <?php } else { ?>
+      <button type="submit" class="btn btn-info">ส่งข้อความ <i class="pe-paper-plane"></i></button>
+      <?php } ?>
     </div>
   </div>
 </form>
