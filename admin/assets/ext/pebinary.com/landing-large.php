ea0dc4dc1e2899b77f551dda4cc9917ca0f53f2a
Phumin Chesdmethee <phumin@sawasdee.org>
2016-10-05 16:14:03 +0700

    Finalise external scripts for both pebinary.com and pebinary.net
    Finalise external scripts for both pebinary.com and pebinary.net

--- admin/assets/ext/pebinary.com/landing-large.php
+++ admin/assets/ext/pebinary.com/landing-large.php
@@ -2,9 +2,6 @@
 
 require_once $_SERVER['DOCUMENT_ROOT'].'/admin/assets/include/config.php';
 
-$referer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
-//echo $referer;
-
 $name = cleanstring($_REQUEST['name']);
 $email = cleanstring($_REQUEST['email']);
 // $message = cleanstring($_REQUEST['message']);
@@ -17,16 +14,40 @@ if (!$email) { echo "Error: No email provided.\n"; exit; }
 
 $from = "From: ".$email;
 $subject = '[pebinary.com] online query from '.$email;
-$body ="From: $name ($email)\nChannel: [pebinary.com] landing/large\n\nMessage:\nNone (by default)\n\n";
+$body ="From: $name ($email)\n\nChannel: [pebinary.com]\n\nSource: landing/large\n\nMessage:\n\n[NO MESSAGES]\n\n";
 $headers = 'From: '.$email."\r\n" .
            'Reply-To: '.$email."\r\n" .
            'X-Mailer: PHP/' . phpversion();
 
-if ($referer == MYHOME) {
+if (false !== stripos($_SERVER['HTTP_REFERER'], "www.pebinary.com")) {
   @mail(MYEMAIL, $subject, $body, $headers);
-  echo "Your message has been sent successfully. We will get back to you shortly.\n";
-  echo "We are now redirecting you back to the main website.\n";
-  header("Location: http://www.pebinary.com/");
-}
+?>
 
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="refresh" content="10;url=http://www.pebinary.com">
+  <title>Thank you for your message &middot; PE BINARY CO., LTD.</title>
+  <link rel="stylesheet" type="text/css" href="http://www.pebinary.com/assets/css/main.css">
+  <link rel="stylesheet" type="text/css" href="http://www.pebinary.com/assets/css/404.css">
+</head>
+<body>
+<main>
+<section class="dialog">
+  <h2>Your message has been sent successfully <i class="pe-check-square-o pe-fw"></i></h2>
+  <p>Thank you for sending us a message. We make sure all messages are taken care seriously.</p>
+  <p class="big">Please expect our reply within a few working days.</p>
+  <p class="small">
+    <a href="http://www.pebinary.com">Click here</a> to go back to the front page <i class="pe-home pe-fw"></i> or
+    <a href="http://www.pebinary.com/contact/">contact us</a> <i class="pe-envelope-o pe-fw"></i> to report this problem.
+  </p>
+  <p class="text-center"><span class="logo1"><i class="pe-logo"></i></span> <span class="logo2">pe</span><span class="logo3">binary</span></p>
+</section>
+</main>
+</body>
+</html>
+
+<?php
+}
 ?>
--- admin/assets/ext/pebinary.net/contact-en.php
+++ admin/assets/ext/pebinary.net/contact-en.php
@@ -2,10 +2,9 @@
 
 require_once $_SERVER['DOCUMENT_ROOT'].'/admin/assets/include/config.php';
 
-$name = cleanstring($_REQUEST['name']);
-$email = cleanstring($_REQUEST['email']);
-$message = cleanstring($_REQUEST['message']);
-// $human = intval($_REQUEST['human']);
+$name = cleanstring($_REQUEST['nameen']);
+$email = cleanstring($_REQUEST['emailen']);
+$message = cleanstring($_REQUEST['messageen']);
 
 if (!$name) { echo "Error: No name provided.\n"; exit; }
 if (!$email) { echo "Error: No email provided.\n"; exit; }
@@ -13,13 +12,13 @@ if (!$message) { echo "Error: No message provided.\n"; exit; }
 // if ($human !== 5) { $error .= "Error: Incorrect answer was submitted"; exit; }
 
 $from = "From: ".$email;
-$subject = '[pebinary.com] online query from '.$email;
-$body ="From: $name ($email)\n\nChannel: [pebinary.com]\n\nSource: contact/modal\n\nMessage:\n\n$message\n\n";
+$subject = '[pebinary.net] online query from '.$email;
+$body ="From: $name ($email)\n\nChannel: [pebinary.net]\n\nSource: contact/form\n\nMessage:\n\n$message\n\n";
 $headers = 'From: '.$email."\r\n" .
            'Reply-To: '.$email."\r\n" .
            'X-Mailer: PHP/' . phpversion();
 
-if (false !== stripos($_SERVER['HTTP_REFERER'], "www.pebinary.com")) {
+if (false !== stripos($_SERVER['HTTP_REFERER'], "www.pebinary.net")) {
   @mail(MYEMAIL, $subject, $body, $headers);
 ?>
 
@@ -27,7 +26,7 @@ if (false !== stripos($_SERVER['HTTP_REFERER'], "www.pebinary.com")) {
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <meta http-equiv="refresh" content="10;url=http://www.pebinary.com">
+  <meta http-equiv="refresh" content="10;url=http://www.pebinary.net">
   <title>Thank you for your message &middot; PE BINARY CO., LTD.</title>
   <link rel="stylesheet" type="text/css" href="http://www.pebinary.com/assets/css/main.css">
   <link rel="stylesheet" type="text/css" href="http://www.pebinary.com/assets/css/404.css">
@@ -39,7 +38,7 @@ if (false !== stripos($_SERVER['HTTP_REFERER'], "www.pebinary.com")) {
   <p>Thank you for sending us a message. We make sure all messages are taken care seriously.</p>
   <p class="big">Please expect our reply within a few working days.</p>
   <p class="small">
-    <a href="http://www.pebinary.com">Click here</a> to go back to the front page <i class="pe-home pe-fw"></i> or
+    <a href="http://www.pebinary.net">Click here</a> to go back to the front page <i class="pe-home pe-fw"></i> or
     <a href="http://www.pebinary.com/contact/">contact us</a> <i class="pe-envelope-o pe-fw"></i> to report this problem.
   </p>
   <p class="text-center"><span class="logo1"><i class="pe-logo"></i></span> <span class="logo2">pe</span><span class="logo3">binary</span></p>
