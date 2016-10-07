1c99c92b042c3ec387d9bd8d398214518f186c9b
Phumin Chesdmethee <phumin@sawasdee.org>
2016-10-07 22:32:06 +0700

    Install captcha
    Install captcha

--- admin/contact.php
+++ admin/contact.php
@@ -6,27 +6,34 @@ require_once INCLUDEADM.'/template.php';
 
 $sent = false;
 
-if (isset($_REQUEST["name"])) {
+if (empty($_SESSION["captcha"])) { $clength = 8; $rText = generateRandom($clength); $_SESSION["captcha"] = $rText; }
+
+if (isset($_REQUEST['captcha'])) {
 
   $name = cleanstring($_REQUEST['name']);
   $email = cleanstring($_REQUEST['email']);
   $message = cleanstring($_REQUEST['message']);
-  $human = intval($_REQUEST['human']);
+  $captcha = cleanstring($_REQUEST['captcha']);
 
   if (!$name) { echo "Error: No name provided.\n"; exit; }
   if (!$email) { echo "Error: No email provided.\n"; exit; }
   if (!$message) { echo "Error: No message provided.\n"; exit; }
-  if (!$human) { echo "Error: No answer to our question.\n"; exit; }
+  if (!$captcha) { echo "Error: No answer to captcha.\n"; exit; }
 
   $from = "From: ".$email;
   $subject = '[siamsquare.org] contact query from '.$email;
-  $body ="From: $name ($email)\n\nChannel: [siamsquare.org]\n\nSource: admin/contact\n\nMessage:\n\n$message\n\n";
+  $body = "----- Header --------------- \n\n";
+  $body .= "From: $name ($email)\n\n";
+  $body .= "Channel: http://www.siamsquare.org\n\n";
+  $body .= "Source: admin/contact\n\n";
+  $body .= "----- Detail --------------- \n\n";
+  $body .= "Message:\n\n$message\n\n";
   $headers = 'From: '.$email."\r\n" .
              'Reply-To: '.$email."\r\n" .
              'X-Mailer: PHP/' . phpversion();
 
-  if ($human == 5) { if (@mail(MYEMAIL, $subject, $body, $headers)) { $msg = mksuccess("Your message has been sent successfully. We will get back to you shortly."); $sent = true; } }
-  else { $msg = mkerror("Your answer to our question is incorrect"); }
+  if ($_REQUEST['captcha'] == $_SESSION['captcha']) { @mail(MYEMAIL, $subject, $body, $headers); $msg = mksuccess("Your message has been sent successfully. We will get back to you shortly."); $sent = true; }
+  else { $msg = mkerror("Your answer to captcha is incorrect"); }
 
 }
 
@@ -40,66 +47,71 @@ echo "<br>\n";
 
 ?>
 
-<form class="form-horizontal" role="form" method="post" action="<?php echo htmlspecialchars(ME); ?>">
-  <div id="contactus">
-    <div class="form-group">
-      <label for="name" class="col-xs-12 col-sm-3 col-md-3 col-lg-3 control-label">Name</label>
-      <div class="col-xs-12 col-sm-7 col-md-7 col-lg-7">
-        <div class="input-group">
-          <span class="input-group-addon"><i class="pe-user pe-fw"></i></span>
-          <?php if ($sent) { ?>
-          <input type="text" class="form-control" name="name" placeholder="First &amp; Last Name" value="<?php echo htmlspecialchars($_REQUEST['name']); ?>" disabled>
-          <?php } else { ?>
-          <input type="text" class="form-control" name="name" placeholder="First &amp; Last Name" value="<?php echo htmlspecialchars($_REQUEST['name']); ?>">
-          <?php } ?>
-        </div>
-      </div>
-    </div>
-    <div class="form-group">
-      <label for="name" class="col-xs-12 col-sm-3 col-md-3 col-lg-3 control-label">Email</label>
-      <div class="col-xs-12 col-sm-7 col-md-7 col-lg-7">
-        <div class="input-group">
-          <span class="input-group-addon"><i class="pe-envelope pe-fw"></i></span>
-          <?php if ($sent) { ?>
-          <input type="email" class="form-control" name="email" placeholder="email@company.com" value="<?php echo htmlspecialchars($_REQUEST['email']); ?>" disabled>
-          <?php } else { ?>
-          <input type="email" class="form-control" name="email" placeholder="email@company.com" value="<?php echo htmlspecialchars($_REQUEST['email']); ?>">
-          <?php } ?>
-        </div>
-      </div>
-    </div>
-    <div class="form-group">
-      <label for="name" class="col-xs-12 col-sm-3 col-md-3 col-lg-3 control-label">Message</label>
-      <div class="col-xs-12 col-sm-7 col-md-7 col-lg-7">
-        <div class="input-group">
-          <span class="input-group-addon input-group-addon-top"><i class="pe-commenting pe-fw"></i></span>
-          <textarea class="form-control" rows="7" name="message" placeholder="Your message can be as long as you need"><?php echo htmlspecialchars($_REQUEST['message']);?></textarea>
-        </div>
-      </div>
-    </div>
-    <div class="form-group">
-      <label for="name" class="col-xs-12 col-sm-3 col-md-3 col-lg-3 control-label">2 + 3 = ?</label>
-      <div class="col-xs-12 col-sm-7 col-md-7 col-lg-7">
-        <div class="input-group">
-          <span class="input-group-addon"><i class="pe-question pe-fw"></i></span>
-          <?php if ($sent) { ?>
-          <input type="text" class="form-control" name="human" placeholder="The answer for 2 + 3 is..." value="<?php echo htmlspecialchars($_REQUEST['human']); ?>" disabled>
-          <?php } else { ?>
-          <input type="text" class="form-control" name="human" placeholder="The answer for 2 + 3 is..." value="<?php echo htmlspecialchars($_REQUEST['human']); ?>">
-          <?php } ?>
-        </div>
-      </div>
-    </div>
-    <div class="form-group">
-      <div class="col-xs-offset-0 col-sm-offset-3 col-md-offset-3 col-lg-offset-3 col-xs-12 col-sm-7 col-md-7 col-lg-7">
+<form id="contactus" class="form-horizontal" role="form" method="post" action="<?php echo htmlspecialchars(ME); ?>">
+  <div class="form-group">
+    <label class="col-xs-12 col-sm-3 col-md-3 col-lg-3 control-label">Name</label>
+    <div class="col-xs-12 col-sm-7 col-md-7 col-lg-7">
+      <div class="input-group">
+        <span class="input-group-addon"><i class="pe-user pe-fw"></i></span>
         <?php if ($sent) { ?>
-        <button type="submit" class="btn btn-warning" disabled>Send <i class="pe-paper-plane"></i></button>
+        <input type="text" class="form-control" name="name" placeholder="First &amp; Last Name" value="<?php echo htmlspecialchars($name); ?>" disabled>
         <?php } else { ?>
-        <button type="submit" class="btn btn-warning">Send <i class="pe-paper-plane"></i></button>
+        <input type="text" class="form-control" name="name" placeholder="First &amp; Last Name" value="<?php echo htmlspecialchars($name); ?>">
         <?php } ?>
       </div>
     </div>
   </div>
+  <div class="form-group">
+    <label class="col-xs-12 col-sm-3 col-md-3 col-lg-3 control-label">Email</label>
+    <div class="col-xs-12 col-sm-7 col-md-7 col-lg-7">
+      <div class="input-group">
+        <span class="input-group-addon"><i class="pe-envelope pe-fw"></i></span>
+        <?php if ($sent) { ?>
+        <input type="email" class="form-control" name="email" placeholder="email@company.com" value="<?php echo htmlspecialchars($email); ?>" disabled>
+        <?php } else { ?>
+        <input type="email" class="form-control" name="email" placeholder="email@company.com" value="<?php echo htmlspecialchars($email); ?>">
+        <?php } ?>
+      </div>
+    </div>
+  </div>
+  <div class="form-group">
+    <label class="col-xs-12 col-sm-3 col-md-3 col-lg-3 control-label">Message</label>
+    <div class="col-xs-12 col-sm-7 col-md-7 col-lg-7">
+      <div class="input-group">
+        <span class="input-group-addon input-group-addon-top"><i class="pe-commenting pe-fw"></i></span>
+        <?php if ($sent) { ?>
+        <textarea class="form-control" rows="7" name="message" placeholder="Your message can be as long as you need" disabled><?php echo htmlspecialchars($message);?></textarea>
+        <?php } else { ?>
+        <textarea class="form-control" rows="7" name="message" placeholder="Your message can be as long as you need"><?php echo htmlspecialchars($message);?></textarea>
+        <?php } ?>
+      </div>
+    </div>
+  </div>
+  <hr>
+  <div class="form-group">
+    <label class="col-xs-12 col-sm-3 col-md-3 col-lg-3 control-label">Confirmation</label>
+    <div class="col-xs-12 col-sm-7 col-md-7 col-lg-7">
+      <p>Please type in the words as shown in this picture<br>
+      <img src="assets/include/captcha.php" alt="captcha"></p>
+      <div class="input-group">
+        <span class="input-group-addon"><i class="pe-cog pe-fw"></i></span>
+        <?php if ($sent) { ?>
+          <input type="text" name="captcha" class="form-control" placeholder="Please type in the words as shown in the picture" value="<?php echo htmlspecialchars($captcha); ?>" disabled>
+        <?php } else { ?>
+        <input type="text" name="captcha" class="form-control" placeholder="Please type in the words as shown in the picture" value="<?php echo htmlspecialchars($captcha); ?>">
+        <?php } ?>
+      </div>
+    </div>
+  </div>
+  <hr>
+  <p class="text-center">
+    <?php if ($sent) { ?>
+    <button type="submit" class="btn btn-warning" disabled>Send <i class="pe-paper-plane"></i></button>
+    <?php } else { ?>
+    <button type="submit" class="btn btn-warning">Send <i class="pe-paper-plane"></i></button>
+    <?php } ?>
+    <button type="submit" class="btn btn-default">Cancel</button>
+  </p>
 </form>
 <script>
   $(document).ready(function() {
@@ -112,7 +124,7 @@ echo "<br>\n";
         name: { validators: { notEmpty: { message: 'Please enter your first and last name' }, } },
         email: { validators: { notEmpty: { message: 'Please enter your email' }, emailAddress: { message: 'Your email is invalid' } } },
         message: { validators: { notEmpty: { message: 'Please type your message' } } },
-        human: { validators: { notEmpty: { message: 'Please enter the answer of this question' }, stringLength: { max: 1, message: 'The answer has to be in one digit' }, regexp: { regexp: /^[0-9]+$/, message: 'The answer has to be in number only' } } }
+        captcha: { validators: { notEmpty: { message: 'Captcha is required' }, stringLength: { min: 8, message: 'Captcha must be 8 characters long' } } }
       }
     });
   });
--- public/contact.php
+++ public/contact.php
@@ -7,22 +7,34 @@ require_once INCLUDEPUB.'/first.php';
 
 $sent = false;
 
-if (isset($_REQUEST["name"])) {
+if (empty($_SESSION["captcha"])) { $clength = 8; $rText = generateRandom($clength); $_SESSION["captcha"] = $rText; }
+
+if (isset($_REQUEST['captcha'])) {
 
   $name = cleanstring($_REQUEST['name']);
   $email = cleanstring($_REQUEST['email']);
   $message = cleanstring($_REQUEST['message']);
-  $human = intval($_REQUEST['human']);
+  $captcha = cleanstring($_REQUEST['captcha']);
+
+  if (!$name) { echo "Error: No name provided.\n"; exit; }
+  if (!$email) { echo "Error: No email provided.\n"; exit; }
+  if (!$message) { echo "Error: No message provided.\n"; exit; }
+  if (!$captcha) { echo "Error: No answer to captcha.\n"; exit; }
+
   $from = "From: ".$email;
-  $subject = '[siamsquare.org] online query from '.$email;
-  $body ="From: $name ($email)\nChannel: [siamsquare.org/public]\n\nMessage:\n$message\n\n";
+  $subject = '[siamsquare.org] contact query from '.$email;
+  $body = "----- Header --------------- \n\n";
+  $body .= "From: $name ($email)\n\n";
+  $body .= "Channel: http://www.siamsquare.org\n\n";
+  $body .= "Source: public/contact\n\n";
+  $body .= "----- Detail --------------- \n\n";
+  $body .= "Message:\n\n$message\n\n";
   $headers = 'From: '.$email."\r\n" .
              'Reply-To: '.$email."\r\n" .
              'X-Mailer: PHP/' . phpversion();
 
-  if ($human == 5) { if (@mail(MYEMAIL, $subject, $body, $headers)) { $msg = mksuccess("ข้อความของคุณได้ถูกส่งเป็นที่เรียบร้อยแล้ว เราจะติดต่อกลับมาหาคุณเร็วๆนี้"); $sent = true; } }
-  else { $msg = mkerror("คุณตอบคำถามในช่องสุดท้ายไม่ถูกต้อง"); }
-
+  if ($_REQUEST['captcha'] == $_SESSION['captcha']) { @mail(MYEMAIL, $subject, $body, $headers); $msg = mksuccess("ข้อความของคุณได้ถูกส่งเป็นที่เรียบร้อยแล้ว เราจะติดต่อกลับมาหาคุณเร็วๆนี้"); $sent = true; }
+  else { $msg = mkerror("คุณใส่ตัวเลขและตัวอักษรตามรูปภาพไม่ถูกต้อง"); }
 }
 
 pageHeader($title);
@@ -35,68 +47,71 @@ echo "<br>\n";
 
 ?>
 
-<form id="contactus" class="form-horizontal" role="form" method="post" action="<?php echo htmlspecialchars(MYPUBLIC); ?>">
+<form id="contactus" class="form-horizontal" role="form" method="post" action="<?php echo htmlspecialchars(ME); ?>">
   <div class="form-group">
-    <label for="name" class="col-xs-12 col-sm-3 col-md-3 col-lg-3 control-label">ชื่อของคุณ</label>
+    <label class="col-xs-12 col-sm-3 col-md-3 col-lg-3 control-label">ชื่อของคุณ</label>
     <div class="col-xs-12 col-sm-7 col-md-7 col-lg-7">
       <div class="input-group">
         <span class="input-group-addon"><i class="pe-user pe-fw"></i></span>
         <?php if ($sent) { ?>
-        <input type="text" class="form-control" name="name" placeholder="ชื่อและนามสกุลของคุณ" value="<?php echo htmlspecialchars($_REQUEST['name']); ?>" disabled>
+        <input type="text" class="form-control" name="name" placeholder="ชื่อและนามสกุลของคุณ" value="<?php echo htmlspecialchars($name); ?>" disabled>
         <?php } else { ?>
-        <input type="text" class="form-control" name="name" placeholder="ชื่อและนามสกุลของคุณ" value="<?php echo htmlspecialchars($_REQUEST['name']); ?>">
+        <input type="text" class="form-control" name="name" placeholder="ชื่อและนามสกุลของคุณ" value="<?php echo htmlspecialchars($name); ?>">
         <?php } ?>
       </div>
     </div>
   </div>
   <div class="form-group">
-    <label for="name" class="col-xs-12 col-sm-3 col-md-3 col-lg-3 control-label">อีเมล์ของคุณ</label>
+    <label class="col-xs-12 col-sm-3 col-md-3 col-lg-3 control-label">อีเมล์ของคุณ</label>
     <div class="col-xs-12 col-sm-7 col-md-7 col-lg-7">
       <div class="input-group">
         <span class="input-group-addon"><i class="pe-envelope pe-fw"></i></span>
         <?php if ($sent) { ?>
-        <input type="email" class="form-control" name="email" placeholder="email@company.com" value="<?php echo htmlspecialchars($_REQUEST['email']); ?>" disabled>
+        <input type="email" class="form-control" name="email" placeholder="อีเมล์@โดเมน.คอม" value="<?php echo htmlspecialchars($email); ?>" disabled>
         <?php } else { ?>
-        <input type="email" class="form-control" name="email" placeholder="email@company.com" value="<?php echo htmlspecialchars($_REQUEST['email']); ?>">
+        <input type="email" class="form-control" name="email" placeholder="อีเมล์@โดเมน.คอม" value="<?php echo htmlspecialchars($email); ?>">
         <?php } ?>
       </div>
     </div>
   </div>
   <div class="form-group">
-    <label for="name" class="col-xs-12 col-sm-3 col-md-3 col-lg-3 control-label">ข้อความ</label>
+    <label class="col-xs-12 col-sm-3 col-md-3 col-lg-3 control-label">ข้อความ</label>
     <div class="col-xs-12 col-sm-7 col-md-7 col-lg-7">
       <div class="input-group">
         <span class="input-group-addon input-group-addon-top"><i class="pe-commenting pe-fw"></i></span>
         <?php if ($sent) { ?>
-        <textarea class="form-control" rows="7" name="message" placeholder="คุณสามารถพิมพ์ข้อความได้ยาวเท่าที่ต้องการในช่องนี้" disabled><?php echo htmlspecialchars($_REQUEST['message']);?></textarea>
+        <textarea class="form-control" rows="7" name="message" placeholder="คุณสามารถพิมพ์ข้อความได้ยาวเท่าที่ต้องการในช่องนี้" disabled><?php echo htmlspecialchars($message);?></textarea>
         <?php } else { ?>
-        <textarea class="form-control" rows="7" name="message" placeholder="คุณสามารถพิมพ์ข้อความได้ยาวเท่าที่ต้องการในช่องนี้"><?php echo htmlspecialchars($_REQUEST['message']);?></textarea>
+        <textarea class="form-control" rows="7" name="message" placeholder="คุณสามารถพิมพ์ข้อความได้ยาวเท่าที่ต้องการในช่องนี้"><?php echo htmlspecialchars($message);?></textarea>
         <?php } ?>
       </div>
     </div>
   </div>
+  <hr>
   <div class="form-group">
-    <label for="name" class="col-xs-12 col-sm-3 col-md-3 col-lg-3 control-label">2 + 3 เท่ากับ?</label>
+    <label class="col-xs-12 col-sm-3 col-md-3 col-lg-3 control-label">ยืนยัน</label>
     <div class="col-xs-12 col-sm-7 col-md-7 col-lg-7">
+      <p>กรุณาพิมพ์ตัวอักษรที่แสดงให้ดูในรูปนี้เพื่อยืนยัน<br>
+      <img src="assets/include/captcha.php" alt="captcha"></p>
       <div class="input-group">
-        <span class="input-group-addon"><i class="pe-question pe-fw"></i></span>
+        <span class="input-group-addon"><i class="pe-cog pe-fw"></i></span>
         <?php if ($sent) { ?>
-        <input type="text" class="form-control" name="human" placeholder="คำตอบของ 2 + 3 คือ..." value="<?php echo htmlspecialchars($_REQUEST['human']); ?>" disabled>
+          <input type="text" name="captcha" class="form-control" placeholder="พิมพ์ตัวอักษรที่แสดงในภาพนี้" value="<?php echo htmlspecialchars($captcha); ?>" disabled>
         <?php } else { ?>
-        <input type="text" class="form-control" name="human" placeholder="คำตอบของ 2 + 3 คือ..." value="<?php echo htmlspecialchars($_REQUEST['human']); ?>">
+        <input type="text" name="captcha" class="form-control" placeholder="พิมพ์ตัวอักษรที่แสดงในภาพนี้" value="<?php echo htmlspecialchars($captcha); ?>">
         <?php } ?>
       </div>
     </div>
   </div>
-  <div class="form-group">
-    <div class="col-xs-offset-0 col-sm-offset-3 col-md-offset-3 col-lg-offset-3 col-xs-12 col-sm-7 col-md-7 col-lg-7">
-      <?php if ($sent) { ?>
-      <button type="submit" class="btn btn-info" disabled>ส่งข้อความ <i class="pe-paper-plane"></i></button>
-      <?php } else { ?>
-      <button type="submit" class="btn btn-info">ส่งข้อความ <i class="pe-paper-plane"></i></button>
-      <?php } ?>
-    </div>
-  </div>
+  <hr>
+  <p class="text-center">
+    <?php if ($sent) { ?>
+    <button type="submit" class="btn btn-info" disabled>ส่งข้อความ <i class="pe-paper-plane"></i></button>
+    <?php } else { ?>
+    <button type="submit" class="btn btn-info">ส่งข้อความ <i class="pe-paper-plane"></i></button>
+    <?php } ?>
+    <button type="submit" class="btn btn-default">Cancel</button>
+  </p>
 </form>
 <script>
   $(document).ready(function() {
@@ -109,7 +124,7 @@ echo "<br>\n";
         name: { validators: { notEmpty: { message: 'กรุณาระบุชื่อและนามสกุลจริงของคุณ' }, } },
         email: { validators: { notEmpty: { message: 'กรุณาระบุอีเมล์ที่ถูกต้อง' }, emailAddress: { message: 'คุณกรอกอีเมล์ไม่ถูกต้อง' } } },
         message: { validators: { notEmpty: { message: 'กรุณาพิมพ์ข้อความที่คุณต้องการส่งถึงเรา' } } },
-        human: { validators: { notEmpty: { message: 'กรุณาพิมพ์คำตอบของคำถามนี้' }, stringLength: { max: 1, message: 'คำตอบจะเป็นเพียงแค่เลขตัวเดียวเท่านั้น' }, regexp: { regexp: /^[0-9]+$/, message: 'คำตอบจะเป็นเพียงแค่ตัวเลขเท่านั้น' } } }
+        captcha: { validators: { notEmpty: { message: 'กรุณาใส่ตัวเลขและตัวอักษรตามรูปภาพ' }, stringLength: { min: 8, message: 'ตัวเลขและตัวอักษรนี้จะต้องมีจำนวนทั้งสิ้น 8 ตัวอักษร' } } }
       }
     });
     $("div.alert").addClass("alert-th");
