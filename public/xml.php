17aa55ccfde7c2611d04524d076b4429198bb898
Phumin Chesdmethee <phumin@sawasdee.org>
2016-10-08 11:08:27 +0700

    Testing out the XML data
    Testing out the XML data

--- public/xml.php
+++ public/xml.php
@@ -10,16 +10,31 @@ pageHeader($title);
 
 // --------------------------------------------------------------------------------
 
-$p = new SimpleXMLElement(file_get_contents("assets/xml/provinces_eng.xml"));
+$p = new SimpleXMLElement(file_get_contents("assets/xml/provinces_th.xml"));
 
-foreach ($p->children() as $ps) {
-  echo $ps->title;
-  echo "<br>\n";
-  foreach ($ps->title->provinces as $ss) {
-    echo $ss->province;
-    echo "<br>\n";
-  }
-}
+echo $p->region->province;
+//print_r($p);
+
+// echo $p->region[0]->province . "<br>";
+// echo $p->region[1]->province;
+//
+// foreach($p->children() as $pp) {
+//     echo $pp->province . ", ";
+//     echo $pp->province . ", ";
+//     // echo $pp->year . ", ";
+//     // echo $pp->price . "<br>";
+// }
+
+// $p = new SimpleXMLElement(file_get_contents("assets/xml/provinces_th.xml"));
+//
+// foreach ($p->children() as $ps) {
+//   echo $ps->title;
+//   echo "<br>\n";
+//   foreach ($ps->title->provinces as $ss) {
+//     echo $ss->province;
+//     echo "<br>\n";
+//   }
+// }
 
 // --------------------------------------------------------------------------------
 
