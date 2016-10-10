be17779f3737d705cb65d388f9aea7b5a7d4e58c
Phumin Chesdmethee <phumin@sawasdee.org>
2016-10-10 22:57:26 +0700

    Move functions to index.inc
    Move functions to index.inc

--- public/index.php
+++ public/index.php
@@ -1,24 +1,16 @@
 <?php
 
-// Add link to reset a forgotten password
-// Add response ID/confirmation number to finished surveys
-
 if (!isset($title)) { $title = 'SiamSquare survey engine: Respondent'; }
 require_once $_SERVER['DOCUMENT_ROOT'].'/admin/assets/include/config.php';
 require_once INCLUDEPUB.'/template.php';
 require_once INCLUDEPUB.'/first.php';
+
 get_current_respondent($respondent);
 pageHeader($title);
-$w = $_REQUEST['w'];
-if ($w) { include(wPublic($w)); }
-else {
-  handleLogin();
-  handleLogout();
-  //handleChangeProfile();
-  handleChangePassword();
-  if (is_session_authenticated()) { paint_authenticated(); }
-  else { paint_non_authenticated(); }
-}
+
+if (empty($w) && isset($_REQUEST['w'])) { $w = $_REQUEST['w']; }
+include(wPublic($w));
+
 if ($notes) { pageFooter($notes); } else { pageFooter(); }
 
 ?>
