32a88a6c3939a0fca10fd9899882e668cdcc8362
Phumin Chesdmethee <phumin@sawasdee.org>
2016-11-02 00:51:05 +0700

    Update path to public
    Update path to public

--- public/index.php
+++ public/index.php
@@ -1,12 +1,14 @@
 <?php
 
-require_once $_SERVER['DOCUMENT_ROOT'].'/admin/assets/include/config.php';
-require_once $_SERVER['DOCUMENT_ROOT'].'/admin/assets/include/template.php';
-require_once $_SERVER['DOCUMENT_ROOT'].'/admin/assets/include/login.class.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/public/assets/include/config.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/public/assets/include/template.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/public/assets/include/login.class.php';
 
 $login = new Login();
 
-if ($login->isUserLoggedIn() != true) { include $_SERVER['DOCUMENT_ROOT'].'/admin/assets/include/w/login.inc'; }
+if ($login->isUserLoggedIn() != true) { include $_SERVER['DOCUMENT_ROOT'].'/public/assets/include/w/login.inc'; }
 else { $w = ''; if (empty($w) && isset($_REQUEST['w'])) { $w = $_REQUEST['w']; } include(w($w)); }
+
+echo "pe";
 
 ?>
