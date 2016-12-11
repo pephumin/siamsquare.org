ae1d7cc54a1e69544061ba1063a013f3681cb332
Phumin Chesdmethee <phumin@sawasdee.org>
2016-12-12 00:21:26 +0700

    Clean up in js folder
    Clean up in js folder

--- admin/assets/js/etc.js
+++ admin/assets/js/etc.js
@@ -38,3 +38,58 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 ga('create', 'UA-82554411-3', 'auto');
 ga('send', 'pageview');
+
+
+// notification
+
+Notification = window.Notification || {};
+Notification = function () {
+  'use strict';
+  var number = 0;
+  var incPosition = 0;
+  var template = function (title, text, image, position) {
+    incPosition = number * 90;
+    number = number + 1;
+    var textHtml = '<div class="ntext">' + text + '</div>';
+    var titleHtml = (!title ? '' : '<div class="ntitle">' + title + '</div>');
+    var imageHtml = (!image ? '' : '<div class="nimg"><img src="' + image + '" width="40" height="40" /></div>');
+    var style;
+    switch (parseInt(position, 10)) {
+      case 1: style = "top:" + incPosition + "px; left:20px;"; break;
+      case 2: style = "top:" + incPosition + "px; right:20px;"; break;
+      case 3: style = "bottom:" + incPosition + "px; right:20px;"; break;
+      case 4: style = "bottom:" + incPosition + "px; left:20px;"; break;
+    }
+    return {
+      id: number,
+      content: '<div class="notification notification-' + number + ' " style="' + style + '">' +
+      imageHtml +
+      '<div class="text">' + titleHtml + textHtml + '</div>' +
+      '</div>'
+    };
+  };
+  var hide = function (id, outAnimation) {
+    var notification = $(document).find('.notification-' + id);
+    notification.addClass(outAnimation);
+    $(document).find('.notification').css('top', '-=90px');
+    number -= 1;
+    setTimeout(function () { notification.remove(); }, 3000);
+  };
+  var create = function (config) {
+    var notification = template(config.title, config.text, config.image, config.position);
+    $(notification.content).addClass('animated ' + config.inAnimation).appendTo('body');
+    setTimeout(function () { hide(notification.id, config.outAnimation); }, config.delay * 1500 || 7000);
+  };
+  return create;
+}();
+
+
+// no right click
+
+function catch_click(e) {
+  if (!e) var e = window.event;
+  var right_click = (e.which ? (e.which == 3) : (e.button == 2));
+  if (right_click) { alert('Right clicking on this page is not allowed.'); return false; }
+}
+document.onmousedown = catch_click;
+if (document.captureEvents) document.captureEvents(Event.MOUSEDOWN);
