642b356d554ca0c754fd3adb1dc07756ad6ef6ce
Phumin Chesdmethee <phumin@sawasdee.org>
2016-12-14 18:32:48 +0700

    Disable right click function
    Disable right click function

--- admin/assets/js/etc.js
+++ admin/assets/js/etc.js
@@ -86,10 +86,10 @@ Notification = function () {
 
 // no right click
 
-function catch_click(e) {
-  if (!e) var e = window.event;
-  var right_click = (e.which ? (e.which == 3) : (e.button == 2));
-  if (right_click) { alert('Right clicking on this page is not allowed.'); return false; }
-}
-document.onmousedown = catch_click;
-if (document.captureEvents) document.captureEvents(Event.MOUSEDOWN);
+// function catch_click(e) {
+//   if (!e) var e = window.event;
+//   var right_click = (e.which ? (e.which == 3) : (e.button == 2));
+//   if (right_click) { alert('Right clicking on this page is not allowed.'); return false; }
+// }
+// document.onmousedown = catch_click;
+// if (document.captureEvents) document.captureEvents(Event.MOUSEDOWN);
